d612eb575c802e474b274de51cc5aa26d6f3a07f
paramat <paramat@users.noreply.github.com>
2017-08-30 02:37:43 +0100

    Add binoculars 5.X series mod with binoculars item to allow zoom
    * Uses the new player object property 'can zoom'.
    * In survival mode, use of zoom requires the binoculars item in
      inventory.
    * Zoom is automatically allowed in creative mode and for players with
      the 'creative' privilege.
    * The binoculars.update_player_property() function is global so can be
       redefined by a mod for alternative behaviour.
    * imported mientst binocular mod from new 5.0 branchs

--- README.md
+++ README.md
@@ -9,10 +9,9 @@ You have to select the binoculars. Then you can use your zoom-Key (default:z).
 This mod revokes from every player the `zoom` priv and grants zoom on holding the item
 you should add an `alwayszoom` priv for admins, which doesnt allow to revoke zoom
 
-This is the first version mod, by Arcelmi at https://github.com/Arcelmi/minetest-mod-binoculars 
-with a forum post about at https://forum.minetest.net/viewtopic.php?f=9&t=15916
-This version if for use only in 0.4.X version from 0.4.15 to 0.4.18 for newer minetest 
-versions you should use the `main-5.0` branch or `master-original-paramat` branch.
+This is the last version mod, by paramat at https://github.com/minetest-game-mods/binoculars
+This version if for use only in 5.0.X versions and up, for older minetest 
+versions you should use the `main-4.0` branch or `master-original-arcelmi` branch.
 
 ### Crafting
 
@@ -44,6 +43,13 @@ mode and for any player with the 'creative' privilege.
 
 ## LICENSE
 
-
 This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. 
 To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
+
+New code is paramat (MIT)
+
+Authors of media (textures)
+---------------------------
+paramat (CC BY-SA 3.0):
+  binoculars_binoculars.png
+
--- init.lua
+++ init.lua
@@ -1,30 +1,69 @@
+-- Mod global namespace
+
+binoculars = {}
+
+
+-- Cache creative mode setting
+
+local creative_mode_cache = minetest.settings:get_bool("creative_mode")
+
+
+-- Update player property
+-- Global to allow overriding
+
+function binoculars.update_player_property(player)
+	local creative_enabled =
+		(creative and creative.is_enabled_for(player:get_player_name())) or
+		creative_mode_cache
+
+	if creative_enabled or
+			player:get_inventory():contains_item("main", "binoculars:binoculars") then
+		player:set_properties({can_zoom = true})
+	else
+		player:set_properties({can_zoom = false})
+	end
+end
+
+
+-- Set player property 'on joinplayer'
+
+minetest.register_on_joinplayer(function(player)
+	binoculars.update_player_property(player)
+end)
+
+
+-- Cyclic update of player property
+
+local function cyclic_update()
+	for _, player in ipairs(minetest.get_connected_players()) do
+		binoculars.update_player_property(player)
+	end
+	minetest.after(4.7, cyclic_update)
+end
+
+minetest.after(4.7, cyclic_update)
+
+
+-- Binoculars item
+
 minetest.register_craftitem("binoculars:binoculars", {
 	description = "Binoculars",
-	inventory_image = "binoculars.png"
+	inventory_image = "binoculars_binoculars.png",
+	stack_max = 1,
+
+	on_use = function(itemstack, user, pointed_thing)
+		binoculars.update_player_property(user)
+	end,
 })
 
--- TODO this method will crash servers of large amount of users connected, so pending changed the way of
-minetest.register_globalstep(function()
-	for _,player in ipairs(minetest.get_connected_players()) do
-		local name = player:get_player_name()
-		local privs = minetest.get_player_privs(name)
-		if not privs.zoom and player:get_wielded_item():get_name()=="binoculars:binoculars" then
-			privs.zoom = true
-			minetest.set_player_privs(name, privs)
-		elseif privs.zoom and player:get_wielded_item():get_name()~="binoculars:binoculars" and not privs.alwayszoom then
-			privs.zoom = nil
-			minetest.set_player_privs(name, privs)
-		end
-	end
-end)
+
+-- Crafting
 
 minetest.register_craft({
 	output = "binoculars:binoculars",
 	recipe = {
-		{"default:glass", "", "default:glass"},
-		{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
-		{"default:steel_ingot", "",  "default:steel_ingot"}
+		{"default:obsidian_glass", "", "default:obsidian_glass"},
+		{"default:bronze_ingot", "default:bronze_ingot", "default:bronze_ingot"},
+		{"default:obsidian_glass", "", "default:obsidian_glass"},
 	}
 })
-
-minetest.register_privilege("alwayszoom", {})
